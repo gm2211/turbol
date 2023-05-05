47709dd40c1bc91abfd468ae16fc3014036de2ca
gm2211 <gmecocci@outlook.com>
2023-05-05 01:26:19 -0400

    Moved flight route addition to other file - not working for now

--- frontend/src/components/map/WorldMap.vue
+++ frontend/src/components/map/WorldMap.vue
@@ -8,10 +8,12 @@
 import mapboxgl from "mapbox-gl";
 import {onMounted} from "vue";
 
+let map = undefined
+
 onMounted(() => {
     mapboxgl.accessToken = "";
 
-    const map = new mapboxgl.Map({
+    map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/dark-v11',
         center: [-96, 37.8],
@@ -19,79 +21,7 @@ onMounted(() => {
         maxZoom: 10,
         pitch: 40
     });
-
-    // San Francisco
-    const origin = [-122.414, 37.776];
-
-    // Washington DC
-    const destination = [-77.032, 38.913];
-
-    // A simple line from origin to destination.
-    const route = {
-        'type': 'FeatureCollection',
-        'features': [
-            {
-                'type': 'Feature',
-                'geometry': {
-                    'type': 'LineString',
-                    'coordinates': [origin, destination]
-                }
-            }
-        ]
-    };
-
-    // A single point that animates along the route.
-    // Coordinates are initially set to origin.
-    const point = {
-        'type': 'FeatureCollection',
-        'features': [
-            {
-                'type': 'Feature',
-                'properties': {},
-                'geometry': {
-                    'type': 'Point',
-                    'coordinates': origin
-                }
-            }
-        ]
-    };
-
-    map.on('load', () => {
-        // Add a source and layer displaying a point which will be animated in a circle.
-        map.addSource('route', {
-            'type': 'geojson',
-            'data': route
-        });
-
-        map.addSource('point', {
-            'type': 'geojson',
-            'data': point
-        });
-
-        map.addLayer({
-            'id': 'route',
-            'source': 'route',
-            'type': 'line',
-            'paint': {
-                'line-width': 2,
-                'line-color': '#007cbf'
-            }
-        });
-
-        map.addLayer({
-            'id': 'point',
-            'source': 'point',
-            'type': 'symbol',
-            'layout': {
-                'icon-image': 'airport',
-                'icon-size': 1.5,
-                'icon-rotate': ['get', 'bearing'],
-                'icon-rotation-alignment': 'map',
-                'icon-allow-overlap': true,
-                'icon-ignore-placement': true
-            }
-        });
-    });
 })
 
+defineExpose({map})
 </script>
--- frontend/src/views/ForecastView.vue
+++ frontend/src/views/ForecastView.vue
@@ -1,8 +1,87 @@
 <template>
-    <WorldMap/>
+    <WorldMap ref="map"/>
 </template>
 
 <script setup lang="ts">
 import WorldMap from "@/components/map/WorldMap.vue";
+import {onMounted, ref} from "vue";
+import mapboxgl from "mapbox-gl";
+
+const map = ref(undefined as mapboxgl.Map)
+
+onMounted(() => {
+// San Francisco
+    const origin = [-122.414, 37.776];
+
+// Washington DC
+    const destination = [-77.032, 38.913];
+
+// A simple line from origin to destination.
+    const route = {
+        'type': 'FeatureCollection',
+        'features': [
+            {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'LineString',
+                    'coordinates': [origin, destination]
+                }
+            }
+        ]
+    };
+
+// A single point that animates along the route.
+// Coordinates are initially set to origin.
+    const point = {
+        'type': 'FeatureCollection',
+        'features': [
+            {
+                'type': 'Feature',
+                'properties': {},
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': origin
+                }
+            }
+        ]
+    };
+
+    map.value.on('load', () => {
+        // Add a source and layer displaying a point which will be animated in a circle.
+        map.value.addSource('route', {
+            'type': 'geojson',
+            'data': route
+        });
+
+        map.value.addSource('point', {
+            'type': 'geojson',
+            'data': point
+        });
+
+        map.value.addLayer({
+            'id': 'route',
+            'source': 'route',
+            'type': 'line',
+            'paint': {
+                'line-width': 2,
+                'line-color': '#007cbf'
+            }
+        });
+
+        map.value.addLayer({
+            'id': 'point',
+            'source': 'point',
+            'type': 'symbol',
+            'layout': {
+                'icon-image': 'airport',
+                'icon-size': 1.5,
+                'icon-rotate': ['get', 'bearing'],
+                'icon-rotation-alignment': 'map',
+                'icon-allow-overlap': true,
+                'icon-ignore-placement': true
+            }
+        });
+    });
+})
 </script>
 
