089bb53e44b40388fd5be9cddfcb03d233fa5529
gm2211 <gmecocci@outlook.com>
2023-05-05 01:21:10 -0400

    Map

--- frontend/src/components/map/WorldMap.vue
+++ frontend/src/components/map/WorldMap.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="map-container">
-        <div id="map"></div>
-    </div>
+    <v-col class="fill-height">
+        <v-card class="fill-height my-card w-100" id="map"></v-card>
+    </v-col>
 </template>
 
 <script setup lang="ts">
@@ -9,15 +9,89 @@ import mapboxgl from "mapbox-gl";
 import {onMounted} from "vue";
 
 onMounted(() => {
-// Replace 'your_mapbox_access_token' with your actual Mapbox access token
-    mapboxgl.accessToken = "sk.eyJ1IjoiZ21lY29jY2kiLCJhIjoiY2xoYTJ0ajlhMGRjMDNzcGh2cjNldmo5YyJ9.XfHe6yRHDm-AM8ePgBKKBA";
+    mapboxgl.accessToken = "";
 
     const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/dark-v11',
         center: [-96, 37.8],
         zoom: 3,
+        maxZoom: 10,
         pitch: 40
     });
+
+    // San Francisco
+    const origin = [-122.414, 37.776];
+
+    // Washington DC
+    const destination = [-77.032, 38.913];
+
+    // A simple line from origin to destination.
+    const route = {
+        'type': 'FeatureCollection',
+        'features': [
+            {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'LineString',
+                    'coordinates': [origin, destination]
+                }
+            }
+        ]
+    };
+
+    // A single point that animates along the route.
+    // Coordinates are initially set to origin.
+    const point = {
+        'type': 'FeatureCollection',
+        'features': [
+            {
+                'type': 'Feature',
+                'properties': {},
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': origin
+                }
+            }
+        ]
+    };
+
+    map.on('load', () => {
+        // Add a source and layer displaying a point which will be animated in a circle.
+        map.addSource('route', {
+            'type': 'geojson',
+            'data': route
+        });
+
+        map.addSource('point', {
+            'type': 'geojson',
+            'data': point
+        });
+
+        map.addLayer({
+            'id': 'route',
+            'source': 'route',
+            'type': 'line',
+            'paint': {
+                'line-width': 2,
+                'line-color': '#007cbf'
+            }
+        });
+
+        map.addLayer({
+            'id': 'point',
+            'source': 'point',
+            'type': 'symbol',
+            'layout': {
+                'icon-image': 'airport',
+                'icon-size': 1.5,
+                'icon-rotate': ['get', 'bearing'],
+                'icon-rotation-alignment': 'map',
+                'icon-allow-overlap': true,
+                'icon-ignore-placement': true
+            }
+        });
+    });
 })
+
 </script>
